bfb710f844f205e2e90626fc11f00528b7296cec
Thomas Reynolds <me@tdreyno.com>
2012-05-21 08:44:08 -0700

    prep release

--- lib/middleman-minify-html.rb
+++ lib/middleman-minify-html.rb
@@ -1,6 +1,6 @@
 require "middleman-core"
 
-::Middleman::Extensions.register(:minify_html, ">= 3.0.0.beta.2") do
+::Middleman::Extensions.register(:minify_html, ">= 3.0.0.beta.3") do
   require "middleman-minify-html/extension"
   ::Middleman::MinifyHtml
 end
