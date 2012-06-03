80c6f4b508f2ca96151c9f107753e668c5103319
Thomas Reynolds <me@tdreyno.com>
2012-06-03 12:21:54 -0700

    Doesn't need extension version any more

--- lib/middleman-minify-html.rb
+++ lib/middleman-minify-html.rb
@@ -1,6 +1,6 @@
 require "middleman-core"
 
-::Middleman::Extensions.register(:minify_html, ">= 3.0.0.beta.3") do
+::Middleman::Extensions.register(:minify_html) do
   require "middleman-minify-html/extension"
   ::Middleman::MinifyHtml
 end
