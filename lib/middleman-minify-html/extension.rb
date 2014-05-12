859112873eb17f3907935977d193160ab358c2fe
Jeffery Utter <jeffutter@sadclown.net>
2014-05-12 07:36:59 -0500

    Allow passing options to htmlcompressor

--- lib/middleman-minify-html/extension.rb
+++ lib/middleman-minify-html/extension.rb
@@ -1,12 +1,14 @@
 module Middleman
   class MinifyHtmlExtension < Extension
-    def initialize(*)
+    option :htmlcompressor_options, nil, 'Options hash to pass to Htmlcompressor'
+    
+    def initialize(app, options_hash={}, &block)
       super
       require 'htmlcompressor'
     end
 
     def after_configuration
-      app.use ::HtmlCompressor::Rack, options.to_h
+      app.use ::HtmlCompressor::Rack, options.htmlcompressor_options.to_h
     end
   end
 end
